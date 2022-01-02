8bec185b0132778788e6ac0648fe79a4711a8326
Ayla1524 <aylasilva1524@outlook.com>
2022-01-02 12:49:47 -0300

    adicionando estilização parte 1

--- src/Sessions/ToEdit/EditAddress.js
+++ src/Sessions/ToEdit/EditAddress.js
@@ -2,7 +2,78 @@ import React, { useState, useEffect } from "react";
 import useForm from "../../Hooks/useForm";
 import axios from "axios";
 import { BASE_URL } from "../../Constants/Url";
+import styled from "styled-components";
 
+const Barra = styled.div`
+  width: 360px;
+  height: 64px;
+  margin: 0 0 16px;
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);
+  background-color: #fff;
+`;
+const H1 = styled.div`
+  width: 175px;
+  height: 44px;
+  margin: 20px 92px 0 54px;
+  padding: 13px 67.5px 12px;
+`;
+const input = styled.div`
+  width: 264px;
+  height: 18px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: #000;
+`;
+const EditarEndereço = styled.div`
+  width: 360px;
+  height: 640px;
+  padding: 0 0 294px;
+`;
+
+const Retangular = styled.div`
+  width: 328px;
+  height: 56px;
+  margin: 8px 0 0;
+  padding: 19px 48px 19px 16px;
+  border-radius: 4px;
+  border: solid 1px #b8b8b8;
+`;
+
+const label = styled.div`
+  width: 78px;
+  height: 18px;
+  margin: 3px 234px 43px 16px;
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.29px;
+  color: #b8b8b8;
+`;
+
+const div = styled.div`
+  display: flex;
+  flex-direction: center;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Quadro = styled.div`
+  width: 328px;
+  height: 42px;
+  padding: 12px 16px;
+  border-radius: 2px;
+  background-color: var(--dark-peach);
+`;
 const EditAddress = () => {
   const { form, onChangeInput, clear } = useForm({
     street: "",
@@ -46,55 +117,81 @@ const EditAddress = () => {
 
   return (
     <div>
-      <form onSubmit={updateAddress}>
-        <input
-          name="street"
-          value={form.street}
-          placeholder={"Rua"}
-          onChange={onChangeInput}
-          required
-        />
+      <Barra>
+        <H1>Endereço</H1>
+      </Barra>
+      <EditarEndereço>
+        <form onSubmit={updateAddress}>
+          <Retangular>
+            <label>Rua:</label> <br></br>
+            <input
+              name="street"
+              value={form.street}
+              placeholder={"Rua Alessandra Vieira"}
+              onChange={onChangeInput}
+              required
+            />
+          </Retangular>
 
-        <input
-          name="number"
-          placeholder={"Numero"}
-          value={form.number}
-          onChange={onChangeInput}
-          required
-          type={"number"}
-        />
+          <Retangular>
+            <label>Número:</label> <br></br>
+            <input
+              name="number"
+              placeholder={"42"}
+              value={form.number}
+              onChange={onChangeInput}
+              required
+              type={"number"}
+            />
+          </Retangular>
 
-        <input
-          name="neighbourhood"
-          placeholder={"Bairro"}
-          value={form.neighbourhood}
-          onChange={onChangeInput}
-          required
-        />
-        <input
-          name="city"
-          placeholder={"Cidade"}
-          value={form.city}
-          onChange={onChangeInput}
-          required
-        />
-        <input
-          name="state"
-          placeholder={"Estado"}
-          value={form.state}
-          onChange={onChangeInput}
-          required
-        />
-        <input
-          name="complement"
-          placeholder={"complemento"}
-          value={form.complement}
-          onChange={onChangeInput}
-          required
-        />
+          <Retangular>
+            <label>Bairro:</label> <br></br>
+            <input
+              name="neighbourhood"
+              placeholder={"Santana"}
+              value={form.neighbourhood}
+              onChange={onChangeInput}
+              required
+            />
+          </Retangular>
+          <Retangular>
+            <label>Cidade:</label> <br></br>
+            <input
+              name="city"
+              placeholder={"São Paulo"}
+              value={form.city}
+              onChange={onChangeInput}
+              required
+            />
+          </Retangular>
 
-        <button>Enviar</button>
-      </form>
+          <Retangular>
+            <label>Estado:</label> <br></br>
+            <input
+              name="state"
+              placeholder={"Estado"}
+              value={form.state}
+              onChange={onChangeInput}
+              required
+            />
+          </Retangular>
+
+          <Retangular>
+            <label>Complemento:</label> <br></br>
+            <input
+              name="complement"
+              placeholder={"Apto./Bloco"}
+              value={form.complement}
+              onChange={onChangeInput}
+              required
+            />
+          </Retangular>
+          <Quadro>
+            <button>Enviar</button>
+          </Quadro>
+        </form>
+      </EditarEndereço>
     </div>
   );
 };
--- src/Sessions/ToEdit/ToEdit.js
+++ src/Sessions/ToEdit/ToEdit.js
@@ -1,84 +1,180 @@
+import axios from "axios";
+import { BASE_URL } from "../../Constants/Url";
+import { goToProfile } from "../../Router/Coordinate";
+import React, { useState, useEffect } from "react";
+import useForm from "../../Hooks/useForm";
+import styled from "styled-components";
+import back from "../../assets/back.png";
 
-import axios from 'axios'
-import { BASE_URL } from '../../Constants/Url'
-import { goToProfile } from '../../Router/Coordinate'
-import React , {useState, useEffect} from 'react';
-import useForm from '../../Hooks/useForm';
+const Title = styled.div`
+  width: 175px;
+  height: 44px;
+  margin: 20px 92px 0 54px;
+  padding: 13px 67.5px 12px;
+`;
+const BackImg = styled.div`
+  width: 23px;
+  height: 24px;
+  margin: 10px 54px 10px 16px;
+  object-fit: contain;
+`;
 
-const ToEdit = () =>{
-  const {form, onChangeInput, clear} = useForm({name:"", email:"", cpf:""})
+const Bar = styled.div`
+  width: 360px;
+  height: 64px;
+  margin: 0 0 16px;
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);
+  background-color: #fff;
+`;
 
-    
+const EditarCadastro = styled.div`
+  width: 360px;
+  height: 640px;
+  padding: 0 0 294px;
+`;
 
+const Reactangle = styled.div`
+  width: 328px;
+  height: 56px;
+  margin: 8px 0 0;
+  padding: 19px 48px 19px 16px;
+  border-radius: 4px;
+  border: solid 1px #b8b8b8;
+`;
 
- const updateProfile = (event) =>{
+const input = styled.div`
+  width: 264px;
+  height: 18px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: #000;
+`;
 
-     event.preventDefault()
-    
+const label = styled.div`
+  width: 78px;
+  height: 18px;
+  margin: 3px 234px 43px 16px;
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.29px;
+  color: #b8b8b8;
+`;
+
+const Retangulo = styled.div`
+  width: 328px;
+  height: 42px;
+  padding: 12px 16px;
+  border-radius: 2px;
+  background-color: var(--dark-peach);
+`;
+
+const button = styled.div`
+  width: 296px;
+  height: 18px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: #000;
+`;
+
+const ToEdit = () => {
+  const { form, onChangeInput, clear } = useForm({
+    name: "",
+    email: "",
+    cpf: "",
+  });
+
+  const updateProfile = (event) => {
+    event.preventDefault();
+
     const body = {
-        name: form.name,
-        email: form.email,
-        cpf: form.cpf,
-        
-    }
+      name: form.name,
+      email: form.email,
+      cpf: form.cpf,
+    };
 
-    axios.put (`${BASE_URL}/profile`,body,{
-        headers:{
-       auth: localStorage.getItem('token')
-        }
-        })
-        .then((response) =>{
-         console.log(response)
-        })
-    
-        .catch((err) => {
-         console.log(err)
-         alert ("Tente novamente!")
+    axios
+      .put(`${BASE_URL}/profile`, body, {
+        headers: {
+          auth: localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        console.log(response);
+      })
 
-        })
-        
-       clear()
+      .catch((err) => {
+        console.log(err);
+        alert("Tente novamente!");
+      });
 
- }
+    clear();
+  };
 
- 
+  return (
+    <EditarCadastro>
+      <Bar>
+        <Title>Editar</Title>
+        <BackImg src={back} alt="back" />
+      </Bar>
 
+      <form onSubmit={updateProfile}>
+        <Reactangle>
+          <label>Nome:</label>
+          <input
+            name="name"
+            value={form.name}
+            placeholder={"Bruna Oliveira"}
+            onChange={onChangeInput}
+            required
+          />
+        </Reactangle>
 
+        <Reactangle>
+          <label>Email:</label>
+          <input
+            name="email"
+            placeholder={"bruna_o@gmail.com"}
+            value={form.email}
+            onChange={onChangeInput}
+            required
+          />
+        </Reactangle>
 
-    return (
-        <div>
-            <form onSubmit={updateProfile}>
-                
-            <input
-             name="name"
-             value={form.name} 
-             placeholder= {"name"} 
-             onChange={onChangeInput}
-             required
-            />
+        <Reactangle>
+          <label>CPF:</label>
+          <input
+            name="cpf"
+            placeholder={"333.333.333-33"}
+            value={form.cpf}
+            onChange={onChangeInput}
+            required
+          />
+        </Reactangle>
 
-            <input
-             name="email"
-             placeholder= {"email"} 
-             value={form.email} 
-             onChange={onChangeInput}
-             required
-            />
+        <Retangulo>
+          <button>Salvar</button>
+          <button>Voltar</button>
+        </Retangulo>
+      </form>
+    </EditarCadastro>
+  );
+};
 
-            <input
-             name="cpf"
-             placeholder= {"cpf"} 
-             value={form.cpf} 
-             onChange={onChangeInput}
-             required
-            />
-              
-            <button>Enviar</button>
-
-            </form>
-            
-        </div>
-    )
-}
-
-export default ToEdit ;
+export default ToEdit;
